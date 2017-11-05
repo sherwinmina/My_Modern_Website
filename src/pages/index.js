5d1e493e92776b2b48b968fbc3ee54563ceb6c40
Sherwin Mina <sherwinmina@yahoo.com>
2017-11-05 13:54:05 -0800

    fixed home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,26 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React, { Component } from 'react'
+import '../styles/Home.scss'
 
 import Particles from 'react-particles-js'
 import Typing from 'react-typing-animation'
 
-const IndexPage = () => (
-  <div>
-    <div className="home-container">
-      <p className="text-overlay">
-        <Typing speed={200}>
-          <h2>Welcome!</h2>
-          <Typing.Backspace count={10} />
-          <p>Thank you for visting</p>
-          <Typing.Delay ms={1000} />
-          <p>and enjoy.</p>
-        </Typing>
-      </p>
-      <Particles />
-    </div>
-  </div>
-)
+class Home extends Component {
+  render() {
+    return (
+      <div className="home-container">
+        <p className="text-overlay">
+          <Typing speed={200}>
+            <h2>Welcome!</h2>
+            <Typing.Backspace count={10} />
+            <p>Thank you for visting</p>
+            <Typing.Delay ms={1000} />
+            <p>and enjoy.</p>
+          </Typing>
+        </p>
+        <Particles />
+      </div>
+    )
+  }
+}
 
-export default IndexPage
+export default Home
